601c269af4ed787860211bf71f510631024b36ec
ToanTran <tranquoctoan.ce@gmail.com>
2022-06-02 21:42:56 +0700

    boilerplate

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mattn/go-colorable"
+)
 
 func main() {
+	gin.DefaultWriter = colorable.NewColorableStdout()
 	router := gin.Default()
+	router.SetTrustedProxies([]string{"192.168.1.2"})
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello world",
 		})
 	})
-	router.Run()
+	router.Run() // Listen for "/" on port 8080
 }
