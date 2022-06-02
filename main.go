21e946325afc662448be945441bb43293dbbfdee
ToanTran <tranquoctoan.ce@gmail.com>
2022-06-02 21:46:05 +0700

    add recipe struct

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mattn/go-colorable"
 )
@@ -15,4 +17,12 @@ func main() {
 		})
 	})
 	router.Run() // Listen for "/" on port 8080
+}
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
