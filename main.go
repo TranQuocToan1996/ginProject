88097fceed1919fa26036845e177890620984109
ToanTran <tranquoctoan.ce@gmail.com>
2022-06-03 23:14:44 +0700

    Add endpoint search and delete

--- main.go
+++ main.go
@@ -7,20 +7,17 @@ import (
 	"github.com/TranQuocToan1996/ginProject/model"
 	"github.com/TranQuocToan1996/ginProject/recipe"
 	"github.com/gin-gonic/gin"
-	"github.com/mattn/go-colorable"
 )
 
 // init will be executed during the startup of application
 func init() {
-	recipe.Recipes = make([]*model.Recipe, 0)
+	recipe.Recipes = []*model.Recipe{}
 	jsonByte, _ := ioutil.ReadFile("recipes.json")
 	_ = json.Unmarshal(jsonByte, &recipe.Recipes)
 }
 
 func main() {
-	gin.DefaultWriter = colorable.NewColorableStdout()
 	router := gin.Default()
-	router.SetTrustedProxies([]string{"192.168.1.2"})
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -30,6 +27,9 @@ func main() {
 
 	router.POST("/recipes", recipe.AddNewRecipe)
 	router.GET("/recipes", recipe.ListRecipes)
+	router.PUT("/recipes:id", recipe.UpdateRecipes)
+	router.DELETE("/recipes:id", recipe.DeleteRecipes)
+	router.GET("/recipes/search", recipe.SearchRecipes)
 
 	router.Run() // Listen for "/" on port 8080
 }
--- recipe/handler.go
+++ recipe/handler.go
@@ -2,6 +2,7 @@ package recipe
 
 import (
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/TranQuocToan1996/ginProject/model"
@@ -34,4 +35,68 @@ func AddNewRecipe(c *gin.Context) {
 // ListRecipes returns a list of recipes in JSON format
 func ListRecipes(c *gin.Context) {
 	c.JSON(http.StatusOK, Recipes)
+}
+
+// UpdateRecipes updates a recipe
+func UpdateRecipes(c *gin.Context) {
+	id := c.Param("id")
+	recipe := &model.Recipe{}
+	if err := c.ShouldBindJSON(recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+	i := 0
+	for ; i < len(Recipes); i++ {
+		if Recipes[i].ID == id {
+			Recipes[i] = recipe
+			break
+		}
+	}
+	if i == len(Recipes)-1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "id not found",
+		})
+	}
+	c.JSON(http.StatusOK, recipe)
+}
+
+// DeleteRecipes deletes a recipe
+func DeleteRecipes(c *gin.Context) {
+	id := c.Param("id")
+	delete := false
+	for i := 0; i < len(Recipes); i++ {
+		if Recipes[i].ID == id {
+			Recipes = append(Recipes[:i], Recipes[i+1:]...)
+			delete = true
+			break
+		}
+	}
+	if !delete {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found"})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Recipe has been deleted",
+		})
+	}
+
+}
+
+func SearchRecipes(c *gin.Context) {
+	tag := c.Query("tag")
+	listOfRecipes := []*model.Recipe{}
+	for i := 0; i < len(Recipes); i++ {
+		found := false
+		for _, t := range Recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				found = true
+			}
+		}
+		if found {
+			listOfRecipes = append(listOfRecipes, Recipes[i])
+		}
+	}
+	c.JSON(http.StatusOK, listOfRecipes)
+
 }
